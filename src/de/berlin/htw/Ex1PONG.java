f200161a78db705dbbb6195d3be3ee56c468de9b
disrupted <salomon.popp@gmail.com>
2017-10-23 14:44:38 +0200

    draw paddles + ball

--- src/de/berlin/htw/Ex1PONG.java
+++ src/de/berlin/htw/Ex1PONG.java
@@ -1,6 +1,7 @@
 package de.berlin.htw;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
@@ -8,6 +9,17 @@ import org.lwjgl.opengl.GL11;
 public class Ex1PONG {
   private static int width = 800;
   private static int height = 640;
+  private int fps, lastFPS;
+  private long lastTime;
+  private static int paddleHeight = 100;
+  private static int paddleThickness = 10;
+  private int player1Pos = height / 2;
+  private int player2Pos = height / 2;
+  private int ballX = width / 2;
+  private int ballY = height / 2;
+  private int ballSize = 20;
+  int speed = 10;
+  int ballspeed = 1;
 
   public Ex1PONG() {
   }
@@ -44,18 +56,35 @@ public class Ex1PONG {
   public boolean update() {
     boolean end = Display.isCloseRequested();
     Display.update();
+    long time = getTime();
+    float timeDiff = time - lastTime; // delta between frames
+    lastTime = time;
+    updateFPS();
+    /* if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) end = true;
+    if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) isRunning = true;
+    if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) xPlayerPos -= xPlayerSpeed * timeDiff;
+    if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) xPlayerPos += xPlayerSpeed * timeDiff; */
     return !end;
   }
 
   public void draw() {
     // clear screen
-    GL11.glClearColor(0.3f, 0.3f, 0.3f, 1);
+    GL11.glClearColor(0.1f, 0.1f, 0.1f, 1);
     GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
     // RGBA set color to blue
-    GL11.glColor3f(0.5f, 0.5f, 1.0f);
+    GL11.glColor3f(1.0f, 1.0f, 1.0f);
 
-    drawRect(100, 200, 400, 600);
+    // Player 1
+    drawRect(0, player1Pos, paddleThickness, player1Pos + paddleHeight);
+
+    // Player 2
+    drawRect(width, player2Pos, width - paddleThickness, player2Pos + paddleHeight);
+
+    // Ball
+    drawRect(ballX, ballY, ballX + ballSize, ballY + ballSize);
+    ballX += ballspeed;
+    ballY += ballspeed;
   }
 
   public void finish() {
@@ -70,6 +99,19 @@ public class Ex1PONG {
     GL11.glVertex2f(xe, ye);
     GL11.glVertex2f(xe, ys);
     GL11.glEnd();
+  }
+
+  private long getTime() {
+    return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+  }
+
+  private void updateFPS() {
+    if (getTime() - lastFPS > 1000) {
+      Display.setTitle("FPS: " + fps);
+      fps = 0; // reset FPS counter
+      lastFPS += 1000;
+    }
+    fps++;
   }
 
 }
