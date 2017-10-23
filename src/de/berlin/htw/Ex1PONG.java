e002d72e5448bf0ffd86031c34190373a787d665
disrupted <salomon.popp@gmail.com>
2017-10-23 14:21:24 +0200

    method structure & drawRect

--- src/de/berlin/htw/Ex1PONG.java
+++ src/de/berlin/htw/Ex1PONG.java
@@ -10,43 +10,66 @@ public class Ex1PONG {
   private static int height = 640;
 
   public Ex1PONG() {
-    // open APP
-    try {
-      Display.setDisplayMode(new DisplayMode(width, height));
-      Display.create();
-
-      // INIT
-      GL11.glPointSize(10);
-      GL11.glClearColor(0.3f, 0.3f, 0.3f, 1);
-
-      // LOOP
-      while (!Display.isCloseRequested()) {
-        Display.update();
-
-        // CLEAR
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-
-        GL11.glBegin(GL11.GL_POINTS);
-        for (int i = 0; i < 1000; i++) {
-          float u = (float) Math.random();
-          float v = (float) Math.random();
-          float x = (float) Math.random();
-          float y = (float) Math.random();
-          GL11.glColor3f(1, 1, 1);
-          GL11.glVertex2f(u * 2.f - 1.f, v * 2.f - 1.f);
-        }
-        GL11.glEnd();
-      }
-
-      Display.destroy();
-    } catch (LWJGLException e) {
-      //e.printStackTrace();
-      System.exit(1);
-    }
   }
 
   public static void main(String[] args) {
     Ex1PONG game = new Ex1PONG();
+    game.run();
+  }
+
+  public void run() {
+    setup();
+    while (update()) {
+      draw();
+    }
+    finish();
+  }
+
+  public void setup() {
+    try {
+      Display.setDisplayMode(new DisplayMode(width, height));
+      Display.create();
+    } catch (LWJGLException e) {
+      e.printStackTrace();
+      System.exit(0);
+    }
+
+    // set OpenGL ortho
+    GL11.glMatrixMode(GL11.GL_PROJECTION);
+    GL11.glLoadIdentity();
+    GL11.glOrtho(0, width, 0, height, 1, -1);
+    GL11.glMatrixMode(GL11.GL_MODELVIEW);
+  }
+
+  public boolean update() {
+    boolean end = Display.isCloseRequested();
+    Display.update();
+    return !end;
+  }
+
+  public void draw() {
+    // clear screen
+    GL11.glClearColor(0.3f, 0.3f, 0.3f, 1);
+    GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+    // RGBA set color to blue
+    GL11.glColor3f(0.5f, 0.5f, 1.0f);
+
+    drawRect(100, 200, 400, 600);
+  }
+
+  public void finish() {
+    Display.destroy();
+    System.out.println("GAME OVER");
+  }
+
+  private void drawRect(int xs, int ys, int xe, int ye) {
+    GL11.glBegin(GL11.GL_QUADS);
+    GL11.glVertex2f(xs, ys);
+    GL11.glVertex2f(xs, ye);
+    GL11.glVertex2f(xe, ye);
+    GL11.glVertex2f(xe, ys);
+    GL11.glEnd();
   }
 
 }
